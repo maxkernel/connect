fa8f5c5d6486ba30bc91e1eff70f8952dda15db9
Andrew Klofas <aklofas@gmail.com>
2012-02-06 23:28:33 -0800

    Added initial deploy script.

---
+++ scripts/fabfile.py
@@ -0,0 +1,31 @@
+from fabric.api import run, settings, env
+from fabric.context_managers import cd
+from fabric.operations import put
+
+from os.path import basename
+
+CONFIG		= {
+	'deploy_path'	:	'.deploy',
+	'default_cmd'	:	'sh .run',
+}
+
+def deploy(path, name=None, cmd=CONFIG['default_cmd'], autostart=True):
+	global CONFIG
+	
+	if name == None:
+		name = basename(path)
+	
+	args = dict(CONFIG.items() + {'name': name, 'cmd': cmd}.items())
+	
+	with settings(warn_only=True):
+		run('mkdir ~/%(deploy_path)s' % args)
+		run('max-client -k')
+		put(path, '~/%(deploy_path)s/%(name)s' % args)
+	
+		if autostart:
+			run('/home/dklofas/Projects/maxkernel/src/utils/max-autostart -- max-client --archive=$HOME/%(deploy_path)s/%(name)s %(cmd)s' % args)
+	
+		run('max-client --daemon --archive=$HOME/%(deploy_path)s/%(name)s %(cmd)s' % args)
+	
+
+
